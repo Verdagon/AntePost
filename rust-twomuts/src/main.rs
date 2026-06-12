e1c4794b0e991026cbc73ebe85a709aa5becc6ea
Evan Ovadia <verdagon_null@verdagon.dev>
2026-06-11 20:53:28 -0400

    twomuts: drop verbose header comments, let the code speak

--- rust-twomuts/src/main.rs
+++ rust-twomuts/src/main.rs
@@ -1,9 +1,3 @@
-// This file deliberately fails to compile. That IS the demonstration:
-// Rust's borrow checker forbids two `&mut` references to the same data
-// at the same time.
-//
-//   error[E0499]: cannot borrow `x` as mutable more than once at a time
-
 fn main() {
     let mut x = 5;
     let a = &mut x;
@@ -11,3 +5,5 @@ fn main() {
     *a = 1;
     *b = 2;
 }
+
+//   error[E0499]: cannot borrow `x` as mutable more than once at a time
--- swift-twomuts/Sources/SwiftTwoMuts/main.swift
+++ swift-twomuts/Sources/SwiftTwoMuts/main.swift
@@ -1,13 +1,9 @@
-// This file deliberately fails to compile. That IS the demonstration:
-// Swift forbids passing the same variable to two `inout` parameters
-// of a single function call.
-//
-//   error: inout arguments are not allowed to alias each other
-//   note: previous aliasing argument
-
 func add(_ a: inout Int, _ b: inout Int) {
     a += b
 }
 
 var x = 5
 add(&x, &x)
+
+//   error: inout arguments are not allowed to alias each other
+//   note: previous aliasing argument
